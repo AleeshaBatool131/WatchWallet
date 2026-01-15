fe225ee10820a0eb6e594f538d10799bda786c28
AleeshaBatool131 <SP24-BSE-131@cuilahore.edu.pk>
2026-01-15 16:48:45 +0500

    Admin Settings

--- WatchWalletWeb/Watch_Wallet_app/admin.py
+++ WatchWalletWeb/Watch_Wallet_app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Expense, Budget
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('name', 'user', 'created_at')
+    search_fields=('name',)
+    list_filter=('user',)
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display=(
+        'title',
+        'user',
+        'category',
+        'amount',
+        'expense_date',
+        'created_at'
+        )
+    list_filter=('category','expense_date')
+    search_fields=('title', 'notes')
+    ordering=('-expense_date',)
+    date_hierarchy = 'expense_date'
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display=(
+        'user',
+        'category',
+        'month',
+        'year',
+        'amount_limit'
+    )
+    list_filter = ('year', 'month')
